502c409350e233613f02e95b8462dccc91b5217d
PrakashBhikadiya <133983536+PrakashBhikadiya@users.noreply.github.com>
2023-05-25 10:15:50 -0400

    hello added

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -16,6 +16,7 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("Prakash");
+        System.out.println("hello");
         // TODO code application logic here
     }
     
