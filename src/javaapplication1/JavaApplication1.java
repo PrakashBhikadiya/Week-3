32c4b11f53de3e40dc3299a1f77a1b17d91fe15a
bhikadpr <bhikadpr@381428B0BF47.shernet.sheridancollege.ca>
2023-05-25 11:27:42 -0400

    I dont know

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -24,8 +24,6 @@ public class JavaApplication1 {
         int b=20;
         int c=a+b;
         System.out.println("sum: " + c);
-        int b=a+c;
-        
         // TODO code application logic here
     }
     
